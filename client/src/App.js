58d548c45e0ba76bf7c3ea3d0803aa8e7e7ab339
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-01-17 18:01:01 -0500

    Adding icons fonts

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <span class="typcn typcn-vendor-apple"></span>
         </a>
       </header>
     </div>
